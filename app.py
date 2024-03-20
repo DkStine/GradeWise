ce4eb42e59359ee88f4a125a6ecce410062d2797
Deepak_Kumar <dkctcak@gmail.com>
2024-03-20 21:06:12 +0530

    Implements PDF to TEXT logic through LLM model

--- app.py
+++ app.py
@@ -22,35 +22,44 @@ GOOGLE_API_KEY = os.getenv("API_KEY")
 genai.configure(api_key = GOOGLE_API_KEY)
 
 # Gemini model selection
-model = genai.GenerativeModel('gemini-pro')
+textModel = genai.GenerativeModel('gemini-pro')
+visionModel = genai.GenerativeModel('gemini-pro-vision')
 
-# # --> PDF2Image
-# poppler_path = r"C:/Users/91993/Downloads/Programs/Release-24.02.0-0/poppler-24.02.0/Library/bin"
+# --> PDF2Image
 
-# #path of pdf 
-# pdf_path = r"./essayPdf/Course Plan Eng Phy.pdf"
-
-
-# pages =convert_from_path(pdf_path = pdf_path, poppler_path = poppler_path)
-# #path to savdthe output in.
-# saving_folder = r"./essayImages/"
-
-# #to count the number of pages
-# counter = 1
-# #iterateover all the pages of the pdf
-# for page in pages:
-#     img_name = f"Page_0{counter}.png"
-#     page.save(os.path.join(saving_folder,img_name), "PNG")
-#     counter += 1
+#path of pdf 
+# pdfPath = r"./essayPdf/Course Plan Eng Phy.pdf"
 
 def pdfToText(pdfPath):
-		with Image.open(pdfPath) as img:
-			width, height = img.size
-			for i in range(height // width + 1):
-				page = img.crop((0, i * width, width, i * width))
-				page.save('./essayImages/{}.png'.format(i))
+	poppler_path = r"C:/Users/91993/Downloads/Programs/Release-24.02.0-0/poppler-24.02.0/Library/bin"
+	pages =convert_from_path(pdf_path= pdfPath, poppler_path = poppler_path)
+	#path to save the output in
+	saving_folder = r"./essayImages/"
 
-		text = ""
+	text = ""
+	#to count the number of pages
+	counter = 1
+	#iterateover all the pages of the pdf
+	for page in pages:
+		img_name = f"Page_{counter}.png"
+		path = os.path.join(saving_folder,img_name)
+		page.save(path, "PNG")
+		img = Image.open(path)
+		textResponse = visionModel.generate_content(["Extract all the text that you see in this image, do not add new things, be accurate.", img], stream=True)
+		textResponse.resolve()
+		# print(textResponse.text)
+		text += textResponse.text
+		counter += 1
+
+	print(text)
+
+pdfToText("./essayPdf/Course Plan Eng Phy.pdf")
+# path = os.path.join(r"./essayImages/", "Page_1.png")
+# img = Image.open(path)
+# textResponse = visionModel.generate_content(["Extract all the text that you see in this image, do not add new things, be accurate.", img], stream=True)
+# textResponse.resolve()
+# print(textResponse.text)  --> Helped in Logic Building
+
 
 # Set a secret key for encrypting session data
 app.secret_key = 'my_secret_key'
@@ -79,22 +88,30 @@ def login():
 		else:
 			return '<p>Login failed</p>'
 
+# Sample implementation of LLM API to generate analysis of text
+essayText = "Technology has become an integral part of our lives, transforming the way we live, work, and communicate. From smartphones and laptops to smart homes and self-driving cars, technology has made our lives more convenient and efficient. It has also opened up new opportunities for innovation, creativity, and economic growth. However, as with any powerful tool, technology also presents challenges, such as privacy concerns, cybersecurity threats, and the potential for job displacement. As we continue to navigate this rapidly evolving landscape, it is important to approach technology with a critical and informed perspective, considering both its benefits and its potential risks."
+response = textModel.generate_content("Given is an essay written by a student, analyze it and give constructive feedback and score out it out of 10 marks. The topic is: " + "Technology" + essayText)
+with open(r'./analysisText/output.txt', 'w+') as fp:
+	fp.write(response.text)
+
 @app.route('/input', methods = ['POST'])
 def input():
 	if request.method == 'POST':
-			uploaded_file = request.files['pdfFile']
-			pdf_path = '/tmp/' + uploaded_file.filename
-			with open(pdf_path, 'wb') as file:
-				file.write(uploaded_file.read())
+			# uploaded_file = request.files['pdfFile']
+			# pdfPath = '/tmp/' + uploaded_file.filename
+			# with open(pdfPath, 'wb') as file:
+			# 	file.write(uploaded_file.read())
 			
-			essayText = pdfToText(pdf_path)
+			# essayText = pdfToText(pdfPath)
+			# <---> Logic for accepting pdf input and converting it into text, that is, essayText would hold a String
 
+		essayText = "Technology has become an integral part of our lives, transforming the way we live, work, and communicate. From smartphones and laptops to smart homes and self-driving cars, technology has made our lives more convenient and efficient. It has also opened up new opportunities for innovation, creativity, and economic growth. However, as with any powerful tool, technology also presents challenges, such as privacy concerns, cybersecurity threats, and the potential for job displacement. As we continue to navigate this rapidly evolving landscape, it is important to approach technology with a critical and informed perspective, considering both its benefits and its potential risks."
+		response = textModel.generate_content("Given is an essay written by a student, analyze it and give constructive feedback and score out it out of 10 marks. The topic is: " + "Technology" + essayText)
+		with open(r'./analysisText/output.txt', 'w+') as fp:
+			fp.write(response.text)
+		return 'Done!'
+	
 
-	# essayText = "Technology has become an integral part of our lives, transforming the way we live, work, and communicate. From smartphones and laptops to smart homes and self-driving cars, technology has made our lives more convenient and efficient. It has also opened up new opportunities for innovation, creativity, and economic growth. However, as with any powerful tool, technology also presents challenges, such as privacy concerns, cybersecurity threats, and the potential for job displacement. As we continue to navigate this rapidly evolving landscape, it is important to approach technology with a critical and informed perspective, considering both its benefits and its potential risks."
-	# response = model.generate_content("Given is an essay written by a student, analyze it and give constructive feedback and score out it out of 10 marks. The topic is: " + "Technology" + essayText)
-	# with open(r'./analysisText/output.txt', 'w+') as fp:
-	# 	fp.write(response.text)
-	# return 'Done!'
 
 
 if __name__ == '__main__':
