b7684327154b3ec3c0b9ae1a70bcd17231ff0143
Deepak_Kumar <dkctcak@gmail.com>
2024-03-20 11:22:51 +0530

    Implements Flask 'GET' and 'POST' logic using sample login form

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+
+# Set a secret key for encrypting session data
+app.secret_key = 'my_secret_key'
+
+# dictionary to store user and password
+users = {
+	'kunal': '1234',
+	'user2': 'password2'
+}
+
+# To render a login form 
+@app.route('/')
+def view_form():
+	return render_template('login.html')
+
+# GET method logic
+@app.route('/handle_get', methods=['GET'])
+def handle_get():
+	if request.method == 'GET':
+		username = request.args['username']
+		password = request.args['password']
+		print(username, password)
+		if username in users and users[username] == password:
+			return '<h1>Welcome!!!</h1>'
+		else:
+			return '<h1>invalid credentials!</h1>'
+	else:
+		return render_template('login.html')
+
+# POST method logic
+@app.route('/handle_post', methods=['POST'])
+def handle_post():
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		print(username, password)
+		if username in users and users[username] == password:
+			return '<h1>Welcome!!!</h1>'
+		else:
+			return '<h1>invalid credentials!</h1>'
+	else:
+		return render_template('login.html')
+
+if __name__ == '__main__':
+	app.run()
